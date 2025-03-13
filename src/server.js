dc2aff5ce8b07a19aec91c4f97ea8e55e87be3e2
EMILLY SAMARA VIEIRA SANTANA <CG3028569@car.ifsp>
2025-03-13 19:45:16 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { aiController } from './controllers/aiController.js';
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.post('/ai', aiController)
+app.post('/message', aiController)
+
+
+app.listen(port, () => {
+  console.log(`App de exemplo esta rodando na porta ${port}`)
+})
+
